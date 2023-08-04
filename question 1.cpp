b2c408c97385eb2f6056f37a57b6a3271b586ad5
0xF11 <fuhrye5@gmail.com>
2023-08-04 11:57:25 -0400

    Create question 1.cpp

---
+++ question 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*
+Quiz time
+
+Question #1
+
+Write a for loop that prints every even number from 0 to 20.
+
+Show Solution
+*/
+
+
+
+int main() 
+{
+	for (int q1{ 0 }; q1 <= 20; q1 += 2)
+	{
+		std::cout << q1 << ' ';
+	}
+
+}
